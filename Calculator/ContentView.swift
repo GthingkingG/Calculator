007fde29e36b227715f48055ca13ae89313cc6a0
main <21thingking@gmail.com>
2025-04-02 09:49:26 +0900

    반복문 활용

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     @State var totalNumber: String = ""
+    private var buttonData: [[String]] = [
+        ["C", "+/-", "%", "/"],
+        ["7", "8", "9", "X"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", "0", ".", "="]
+    ]
     var body: some View {
         ZStack{
             Color.black.ignoresSafeArea()
@@ -22,250 +29,28 @@ struct ContentView: View {
                         .font(.system(size: 60))
                         .foregroundColor(.white)
                 }
-                HStack {
-                    Button {
-                        totalNumber = "0"
-                    } label: {
-                        Text("C")
-                            .frame(width: 80, height: 80)
-                            .background(.gray)
-                            .clipShape(Circle())
-                            .foregroundColor(.black)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        
-                    } label: {
-                        Text("+/-")
-                            .frame(width: 80, height: 80)
-                            .background(.gray)
-                            .clipShape(Circle())
-                            .foregroundColor(.black)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        
-                    } label: {
-                        Text("%")
-                            .frame(width: 80, height: 80)
-                            .background(.gray)
-                            .clipShape(Circle())
-                            .foregroundColor(.black)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        
-                    } label: {
-                        Text("/")
-                            .frame(width: 80, height: 80)
-                            .background(.orange)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                }
-                HStack {
-                    Button {
-                        if totalNumber == "0" {
-                            totalNumber = "7"
-                        } else {
-                            totalNumber += "7"
+                VStack {
+                    ForEach(buttonData, id: \.self) {
+                        line in
+                        HStack {
+                            ForEach(line, id: \.self)  {
+                                row in
+                                Button {
+                                    if totalNumber == "0" {
+                                        totalNumber = "7"
+                                    } else {
+                                        totalNumber += "7"
+                                    }
+                                } label: {
+                                    Text(row)
+                                        .frame(width: 80, height: 80)
+                                        .background(.numberButton)
+                                        .clipShape(Circle())
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 33))
+                                }
+                            }
                         }
-                    } label: {
-                        Text("7")
-                            .frame(width: 80, height: 80)
-                            .background(.numberButton)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        if totalNumber == "0" {
-                            totalNumber = "8"
-                        } else {
-                            totalNumber += "8"
-                        }
-                    } label: {
-                        Text("8")
-                            .frame(width: 80, height: 80)
-                            .background(.numberButton)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        if totalNumber == "0" {
-                            totalNumber = "9"
-                        } else {
-                            totalNumber += "9"
-                        }
-                    } label: {
-                        Text("9")
-                            .frame(width: 80, height: 80)
-                            .background(.numberButton)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        
-                    } label: {
-                        Text("X")
-                            .frame(width: 80, height: 80)
-                            .background(.orange)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                }
-                HStack {
-                    Button {
-                        if totalNumber == "0" {
-                            totalNumber = "4"
-                        } else {
-                            totalNumber += "4"
-                        }
-                    } label: {
-                        Text("4")
-                            .frame(width: 80, height: 80)
-                            .background(.numberButton)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        if totalNumber == "0" {
-                            totalNumber = "5"
-                        } else {
-                            totalNumber += "5"
-                        }
-                    } label: {
-                        Text("5")
-                            .frame(width: 80, height: 80)
-                            .background(.numberButton)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        if totalNumber == "0" {
-                            totalNumber = "6"
-                        } else {
-                            totalNumber += "6"
-                        }
-                    } label: {
-                        Text("6")
-                            .frame(width: 80, height: 80)
-                            .background(.numberButton)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        
-                    } label: {
-                        Text("-")
-                            .frame(width: 80, height: 80)
-                            .background(.orange)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                }
-                HStack {
-                    Button {
-                        if totalNumber == "0" {
-                            totalNumber = "1"
-                        } else {
-                            totalNumber += "1"
-                        }
-                    } label: {
-                        Text("1")
-                            .frame(width: 80, height: 80)
-                            .background(.numberButton)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        if totalNumber == "0" {
-                            totalNumber = "2"
-                        } else {
-                            totalNumber += "2"
-                        }
-                    } label: {
-                        Text("2")
-                            .frame(width: 80, height: 80)
-                            .background(.numberButton)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        if totalNumber == "0" {
-                            totalNumber = "3"
-                        } else {
-                            totalNumber += "3"
-                        }
-                    } label: {
-                        Text("3")
-                            .frame(width: 80, height: 80)
-                            .background(.numberButton)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        
-                    } label: {
-                        Text("+")
-                            .frame(width: 80, height: 80)
-                            .background(.orange)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                }
-                HStack {
-                    Button {
-                        totalNumber += "0"
-                    } label: {
-                        Text("0")
-                            .frame(width: 80, height: 80)
-                            .background(.numberButton)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        totalNumber += "0"
-                    } label: {
-                        Text("0")
-                            .frame(width: 80, height: 80)
-                            .background(.numberButton)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        
-                    } label: {
-                        Text(".")
-                            .frame(width: 80, height: 80)
-                            .background(.numberButton)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
-                    }
-                    Button {
-                        
-                    } label: {
-                        Text("=")
-                            .frame(width: 80, height: 80)
-                            .background(.orange)
-                            .clipShape(Circle())
-                            .foregroundColor(.white)
-                            .font(.system(size: 33))
                     }
                 }
             }
